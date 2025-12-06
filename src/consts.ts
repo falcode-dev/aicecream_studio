3da970e80e1593ca7a97fd47d76db083540cbd23
aice <falcode.business@gmail.com>
2025-12-06 16:21:26 +0900

    Japanese localization

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ジェニアス・クレア株式会社';
+export const SITE_DESCRIPTION = 'ジェニアス・クレア株式会社のウェブサイトへようこそ';
