de3e18512f7247df6112af8170a51a7d0c36a505
aice <falcode.business@gmail.com>
2025-12-12 19:38:33 +0900

    recreate project

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// GitHub Pagesのベースパス
-// リポジトリ名がURLに含まれる場合: '/repository-name/'
-// カスタムドメインの場合: '/'
-const base = '/aicecream_studio/';
-
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://falcode-dev.github.io',
-	base: base,
+	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
